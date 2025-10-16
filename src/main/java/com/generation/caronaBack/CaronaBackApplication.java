af9e86b661a4ded0ff20dec2b4c3bd6ed0cac2a6
Jamila Moraes Cardoso <jmc18.ads@gmail.com>
2025-10-16 10:11:50 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/generation/caronaBack/CaronaBackApplication.java
@@ -0,0 +1,13 @@
+package com.generation.caronaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/caronaBack/CaronaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.caronaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
